5f3658e5f96ab9b154dde43726fc5cbb05873bd9
𝗞ʌͥᴍͣɪͫɪ 個 👿 <164133139+kamiqazi@users.noreply.github.com>
2025-02-03 17:08:53 +0500

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`˚˖𓍢ִ໋🌷͙֒✧˚. 𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n🌘-`♡´- 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n⸼𓍢𓈒🌷🧺*:･ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n` + users.map(v => '🪻: ̗̀➛ @' + v.replace(/@.+/, '')).join`\n` + '\n© ꯴⃘፝𝗔𝗬α͜͡𝗭ꨴ🖤꤬ິ̶', null, {
+    m.reply(`┃🤍ᮢᮡ⃝᳧ᩙ╼ׅٜٜ፝֟᷼͠─ܹׄ  𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n┃🍷ᮢᮡ⃝᳧ᩙ᪤̸̷ׅື╼ׅٜٜ፝֟᷼͠─ܹׄ   𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n┃💌ᮢᮡ⃝᳧ᩙ╼ׅٜٜ፝֟᷼͠─ܹׄ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n` + users.map(v => '┃🪻ᮢᮡ⃝᳧ᩙ╼ׅٜٜ፝֟᷼͠─ܹׄ  @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ ⏤͟͟͞➣⃟⃟🍒𝚫𝐘𝚫𝚭~ ⃟⃟⃟⃟<♥️ ┃ ᴮᴼᵀ ✪──', null, {
         mentions: users
     })
 }
