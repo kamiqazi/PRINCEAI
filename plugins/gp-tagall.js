909f67cfccd5b71adf947109d4683af255d44e52
𝗞ʌͥᴍͣɪͫɪ 個 👿 <164133139+kamiqazi@users.noreply.github.com>
2025-02-03 11:36:23 +0500

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,14 +1,13 @@
-let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`┃💗⊹ 𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n┃💗⊹ 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n┃💗⊹ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n` + users.map(v => '┃💗⊹ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ ⚡𝑷-𝑴𝑫⚡ ┃ ᴮᴼᵀ ✪──', null, {
+let handler = async (m, { conn, text, participants, groupMetadata }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
+    m.reply(`┃꤬ິ̶ꨴ🪻꤬ິ̶ 𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n┃꤬ິ̶ꨴ🪻꤬ິ̶ 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n┃꤬ິ̶ꨴ🪻꤬ິ̶ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n` + users.map(v => '┃꤬ິ̶ꨴ🪻꤬ິ̶ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ ꯴⃘፝𝗔𝗬α͜͡𝗭ꨴ🖤꤬ິ̶ ┃ ᴮᴼᵀ ✪──', null, {
         mentions: users
-    })
-}
+    });
+};
 
-handler.help = ['tagall']
-handler.tags = ['group']
-handler.command = ['tagall']
-handler.admin = true
-handler.group = true
+handler.help = ['tagall'];
+handler.tags = ['group'];
+handler.command = ['tagall'];
+handler.group = true;
 
-export default handler
+export default handler;
